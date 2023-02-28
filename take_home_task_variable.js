8e15b8e0f7c47ad6cc9dc524294d7b0a479d7a25
Irfiyandaabidin <irfidupang@gmail.com>
2023-03-01 01:09:20 +0700

    take home task variable

---
+++ take_home_task_variable.js
@@ -0,0 +1,217 @@
+let angka1 = 34;
+let angka2 = 12;
+let angka3 = 11;
+let angka4 = 6;
+let angka5 = 4;
+
+let penjumlahan = angka2 + angka4 + angka1 + angka3 + angka5;
+let pengurangan = angka1 - angka4 - angka5 - angka2 - angka3;
+let perkalian = angka4 * angka5 * angka3 * angka2 * angka1;
+let pembagian = (angka1 + angka2 + angka3) / angka4 / angka5;
+let sisaBagi= (angka1 + angka2 + angka5 + angka3) % angka4;
+let penjumlahanPengurangan = angka2 + angka4 + angka1 - angka3 - angka5;
+let penguranganPerkalian = (angka1 - angka4 - angka5) * angka2 * angka3;
+let perkalianSisabagi = angka4 * angka5 * angka3 * angka2 % angka1;
+let pembagianPerkalian = angka1 / angka4 * angka2 * angka3 * angka5;
+let pangkat = (angka1 + angka2 + angka3 + angka4) ** angka5;
+
+console.log('=====', ' Operator Aritmatika ', '=====')
+console.log(`${angka2} + ${angka4} + ${angka1} + ${angka3} + ${angka5} \t= ${penjumlahan}`)
+console.log(`${angka1} - ${angka4} - ${angka5} - ${angka2} - ${angka3} \t= ${pengurangan}`)
+console.log(`${angka4} * ${angka5} * ${angka3} * ${angka2} * ${angka1} \t= ${perkalian}`)
+console.log(`(${angka1} + ${angka2} + ${angka3}) / ${angka4} / ${angka5} \t= ${pembagian}`)
+console.log(`(${angka1} + ${angka2} + ${angka5} + ${angka3}) % ${angka4} \t= ${sisaBagi}`)
+console.log(`${angka2} + ${angka4} + ${angka1} - ${angka3} - ${angka5} \t= ${penjumlahanPengurangan}`)
+console.log(`(${angka1} - ${angka4} - ${angka5}) * ${angka2} * ${angka3} \t= ${penguranganPerkalian}`)
+console.log(`${angka4} * ${angka5} * ${angka3} * ${angka2} % ${angka1} \t= ${perkalianSisabagi}`)
+console.log(`(${angka1} / ${angka4} * ${angka3}) * ${angka2} * ${angka5} \t= ${pembagianPerkalian}`)
+console.log(`(${angka1} + ${angka2} + ${angka3} + ${angka4}) ** ${angka5} = ${pangkat} \n`)
+
+
+console.log('=====', ' Operator Perbandingan Lebih Dari ( > )', '=====')
+let perbandingan1  = 74 > 53;
+let perbandingan2  = 63 > 25;
+let perbandingan3  = 73 > 51;
+let perbandingan4  = 53 > 81;
+let perbandingan5  = 51 > 92;
+let perbandingan6  = 17 > 23;
+let perbandingan7  = 84 > 42;
+let perbandingan8  = 23 > 48;
+let perbandingan9  = 20 > 89;
+let perbandingan10 = 42 > 37;
+
+console.log(`74 > 53 = ${perbandingan1}`);
+console.log(`63 > 25 = ${perbandingan2}`);
+console.log(`73 > 51 = ${perbandingan3}`);
+console.log(`53 > 81 = ${perbandingan4}`);
+console.log(`51 > 92 = ${perbandingan5}`);
+console.log(`17 > 23 = ${perbandingan6}`);
+console.log(`84 > 42 = ${perbandingan7}`);
+console.log(`23 > 48 = ${perbandingan8}`);
+console.log(`20 > 89 = ${perbandingan9}`);
+console.log(`42 > 37 = ${perbandingan10} \n`);
+
+console.log('=====', ' Operator Perbandingan Kurang Dari ( < )', '=====')
+let perbandingan11 = 86 < 89;
+let perbandingan12 = 31 < 38;
+let perbandingan13 = 18 < 73;
+let perbandingan14 = 93 < 21;
+let perbandingan15 = 12 < 42;
+let perbandingan16 = 32 < 93;
+let perbandingan17 = 83 < 42;
+let perbandingan18 = 91 < 27;
+let perbandingan19 = 31 < 17;
+let perbandingan20 = 12 < 13;
+
+console.log(`86 < 89 = ${perbandingan11}`);
+console.log(`31 < 38 = ${perbandingan12}`);
+console.log(`18 < 73 = ${perbandingan13}`);
+console.log(`93 < 21 = ${perbandingan14}`);
+console.log(`12 < 42 = ${perbandingan15}`);
+console.log(`32 < 93 = ${perbandingan16}`);
+console.log(`83 < 42 = ${perbandingan17}`);
+console.log(`91 < 27 = ${perbandingan18}`);
+console.log(`31 < 17 = ${perbandingan19}`);
+console.log(`12 < 13 = ${perbandingan20}\n`);
+
+console.log('=====', ' Operator Perbandingan Lebih Dari Sama Dengan ( >= )', '=====')
+let perbandingan21 = 834 >= 481;
+let perbandingan22 = 428 >= 394;
+let perbandingan23 = 372 >= 372;
+let perbandingan24 = 324 >= 308;
+let perbandingan25 = 842 >= 502;
+let perbandingan26 = 538 >= 539;
+let perbandingan27 = 318 >= 401;
+let perbandingan28 = 359 >= 427;
+let perbandingan29 = 538 >= 640;
+let perbandingan30 = 429 >= 350;
+
+console.log(`834 >= 481 = ${perbandingan21}`);
+console.log(`428 >= 394 = ${perbandingan22}`);
+console.log(`372 >= 372 = ${perbandingan23}`);
+console.log(`324 >= 308 = ${perbandingan24}`);
+console.log(`842 >= 502 = ${perbandingan25}`);
+console.log(`538 >= 539 = ${perbandingan26}`);
+console.log(`318 >= 401 = ${perbandingan27}`);
+console.log(`359 >= 427 = ${perbandingan28}`);
+console.log(`538 >= 640 = ${perbandingan29}`);
+console.log(`429 >= 350 = ${perbandingan30}\n`);
+
+
+console.log('=====', ' Operator Perbandingan Kurang Dari Sama Dengan ( <= )', '=====')
+let perbandingan31 = 692 <= 931;
+let perbandingan32 = 896 <= 921;
+let perbandingan33 = 485 <= 820;
+let perbandingan34 = 776 <= 173;
+let perbandingan35 = 936 <= 844;
+let perbandingan36 = 949 <= 528;
+let perbandingan37 = 629 <= 723;
+let perbandingan38 = 855 <= 620;
+let perbandingan39 = 973 <= 782;
+let perbandingan40 = 925 <= 645;
+
+console.log(`692 <= 931 = ${perbandingan31}`);
+console.log(`896 <= 921 = ${perbandingan32}`);
+console.log(`485 <= 820 = ${perbandingan33}`);
+console.log(`776 <= 173 = ${perbandingan34}`);
+console.log(`936 <= 844 = ${perbandingan35}`);
+console.log(`949 <= 528 = ${perbandingan36}`);
+console.log(`629 <= 723 = ${perbandingan37}`);
+console.log(`855 <= 620 = ${perbandingan38}`);
+console.log(`973 <= 782 = ${perbandingan39}`);
+console.log(`925 <= 645 = ${perbandingan40}\n`);
+
+console.log('=====', ' Operator Perbandingan Sama Dengan ( == )', '=====')
+let perbandingan51 = 853 == 114;
+let perbandingan52 = 643 == 643;
+let perbandingan53 = 363 == '363';
+let perbandingan54 = 428 == 843;
+let perbandingan55 = 862 == 862;
+let perbandingan56 = 846 == '846';
+let perbandingan57 = 381 == 428;
+let perbandingan58 = 193 == 193;
+let perbandingan59 = 748 == 135;
+let perbandingan60 = 146 == 426;
+
+console.log(`853 == 114 = ${perbandingan51}`);
+console.log(`643 == 643 = ${perbandingan52}`);
+console.log(`363 == '363' = ${perbandingan53}`);
+console.log(`428 == 843 = ${perbandingan54}`);
+console.log(`862 == 862 = ${perbandingan55}`);
+console.log(`846 == '846' = ${perbandingan56}`);
+console.log(`381 == 428 = ${perbandingan57}`);
+console.log(`193 == 193 = ${perbandingan58}`);
+console.log(`748 == 135 = ${perbandingan59}`);
+console.log(`146 == 426 = ${perbandingan60}\n`);
+
+
+console.log(' Operator Perbandingan Sama Dengan Sama Tipe ( === )')
+let perbandingan61 = 247 === '247';
+let perbandingan62 = 486 === 673;
+let perbandingan63 = 747 === 747;
+let perbandingan64 = 264 === 264;
+let perbandingan65 = 739 === 848;
+let perbandingan66 = 332 === 332;
+let perbandingan67 = 291 === '291';
+let perbandingan68 = 813 === 354;
+let perbandingan69 = 718 === 314;
+let perbandingan70 = 391 === 391;
+
+console.log(`247 === '247'= ${perbandingan61}`);
+console.log(`486 === 673 = ${perbandingan62}`);
+console.log(`747 === 7473= ${perbandingan63}`);
+console.log(`264 === 264 = ${perbandingan64}`);
+console.log(`739 === 848 = ${perbandingan65}`);
+console.log(`332 === 3326= ${perbandingan66}`);
+console.log(`291 === '291'= ${perbandingan67}`);
+console.log(`813 === 354 = ${perbandingan68}`);
+console.log(`718 === 314 = ${perbandingan69}`);
+console.log(`391 === 391 = ${perbandingan70}\n`);
+
+
+console.log('=====',' Operator Perbandingan Tidak Sama Dengan ( === )' , '=====')
+let perbandingan71 = 732 != 853;
+let perbandingan72 = 131 != '136';
+let perbandingan73 = 646 != 864;
+let perbandingan74 = 759 != 573;
+let perbandingan75 = 832 != 832;
+let perbandingan76 = 427 != '427';
+let perbandingan77 = 173 != 729;
+let perbandingan78 = 316 != 316;
+let perbandingan79 = 713 != 684;
+let perbandingan80 = 134 != 315;
+
+console.log(`732 != 853 = ${perbandingan71}`);
+console.log(`131 != '136' = ${perbandingan72}`);
+console.log(`646 != 864 = ${perbandingan73}`);
+console.log(`759 != 573 = ${perbandingan74}`);
+console.log(`832 != 832 = ${perbandingan75}`);
+console.log(`427 != '427'= ${perbandingan76}`);
+console.log(`173 != 729 = ${perbandingan77}`);
+console.log(`316 != 316 = ${perbandingan78}`);
+console.log(`713 != 684 = ${perbandingan79}`);
+console.log(`134 != 315 = ${perbandingan80}\n`);
+
+console.log('=====', 'Operator Perbandingan Sama Dengan Sama Tipe ( === )', '=====')
+let perbandingan81 = 642 !== 742;
+let perbandingan82 = 136 !== '136';
+let perbandingan83 = 861 !== 861;
+let perbandingan84 = 427 !== 136;
+let perbandingan85 = 173 !== 173;
+let perbandingan86 = 428 !== '428';
+let perbandingan87 = 635 !== 823;
+let perbandingan88 = 193 !== 427;
+let perbandingan89 = 463 !== 832;
+let perbandingan90 = 197 !== 197;
+
+console.log(`642 !== 742 = ${perbandingan81}`);
+console.log(`136 !== '136' = ${perbandingan82}`);
+console.log(`861 !== 861 = ${perbandingan83}`);
+console.log(`427 !== 136 = ${perbandingan84}`);
+console.log(`173 !== 173 = ${perbandingan85}`);
+console.log(`428 !== '428'= ${perbandingan86}`);
+console.log(`635 !== 823 = ${perbandingan87}`);
+console.log(`193 !== 427 = ${perbandingan88}`);
+console.log(`463 !== 832 = ${perbandingan89}`);
+console.log(`197 !== 197 = ${perbandingan90}\n`);
+
